143a2f6d4df8ef4e879aba3696bb6d29bc02e175
anilkharatmol <akharatmol8@gmail.com>
2024-02-08 23:37:27 +0530

    Retrieved data from database

--- Dynamic_Routes/controllers/admin.js
+++ Dynamic_Routes/controllers/admin.js
@@ -1,10 +1,10 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/edit-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    editing:false
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false,
   });
 };
 
@@ -13,62 +13,80 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null,title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const product = new Product(null, title, price, description, imageUrl);
+  product
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {
-  const editMode= req.query.edit;
+  const editMode = req.query.edit;
 
-  if(!editMode){
-   return res.redirect('/')
+  if (!editMode) {
+    return res.redirect("/");
   }
 
-  const prodId=req.params.productId;
-  Product.findById(prodId,product=>{
-    
-    if(!product){
-      return res.redirect('/')
-     }
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    if (!product) {
+      return res.redirect("/");
+    }
 
-     
-    res.render('admin/edit-product', {
-      product:product,
-      pageTitle: 'Add Product',
-      path:'/admin/add-product',
-      editing: editMode
-    });
-  })
-};
-
-exports.postEditProduct =(req,res,next)=>{
-  const prodId=req.body.productId;
-  const updatedTitle=req.body.title;
-  const updatedPrice=req.body.price;
-  const updatedUrl=req.body.imageUrl;
-  const updatedDescription=req.body.description;
-
-  const updatedProduct=new Product(prodId,updatedTitle,updatedUrl,updatedDescription,updatedPrice)
-
-  updatedProduct.save();
-
-  res.redirect('/admin');
-}
-
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('admin/products', {
-      prods: products,
-      pageTitle: 'Admin Products',
-      path: '/admin/products'
+    res.render("admin/edit-product", {
+      product: product,
+      pageTitle: "Add Product",
+      path: "/admin/add-product",
+      editing: editMode,
     });
   });
 };
 
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedUrl,
+    updatedDescription,
+    updatedPrice
+  );
+
+  updatedProduct.save();
+
+  res.redirect("/admin");
+};
+
+exports.getProducts = (req, res, next) => {
+
+  Product.fetchAll()
+  .then(([rows]) => {
+    res.render("admin/products", {
+      prods: rows,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
+    });
+  })
+  .catch((err) => console.log(err));
+};
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.deleteById(prodId)
+    .then(() => {
+
+      res.redirect("/admin/products");
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 };
--- Dynamic_Routes/controllers/shop.js
+++ Dynamic_Routes/controllers/shop.js
@@ -1,65 +1,68 @@
-const Product = require('../models/product');
-const Cart=require('../models/cart')
+const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
-      prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows]) => {
+      res.render("shop/product-list", {
+        prods: rows,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
-exports.getProductDetails=(req,res,next)=>{
-  const prodId= req.params.productId;
+exports.getProductDetails = (req, res, next) => {
+  const prodId = req.params.productID;
 
- Product.findById(prodId,product=>{
-    res.render('shop/product-detail',{
-      product:product, 
-      pageTitle:product.title,
-      path:'/products'
-    })
- })
-
-}
+  Product.findById(prodId).then(([product])=>{
+    res.render("shop/product-detail", {
+      product: product[0],
+      pageTitle: product.title,
+      path: "/products",
+    });
+  }).catch((e)=>{console.log(e);});
+};
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows]) => {
+      res.render("shop/index", {
+        prods: rows,
+        pageTitle: "Shop",
+        path: "/",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
   });
 };
 
-exports.postCart =(req,res,next)=>{
-  const prodId=req.body.productId;
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
 
-  Product.findById(prodId,(prod)=>{
-    Cart.addProduct(prodId,prod.price)
-  })
-  res.redirect('/cart');
-}
+  Product.findById(prodId, (prod) => {
+    Cart.addProduct(prodId, prod.price);
+  });
+  res.redirect("/cart");
+};
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
   });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
   });
 };
